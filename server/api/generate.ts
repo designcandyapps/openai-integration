a630e720cd9ef11f1de7b7046b1ca56ef72ff189
Erin Dunn Halligan <erin@designcandy.com>
2025-01-12 05:34:48 -0600

    Update generate.ts

--- server/api/generate.ts
+++ server/api/generate.ts
@@ -1,18 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
-
 const runtimeConfig = useRuntimeConfig();
 const { OPENAI_API_KEY } = runtimeConfig;
-
-if ( ! OPENAI_API_KEY ) {
-    console.log("No OpenAI API key found. Please set the OPENAI_API_KEY environment variable.");
-}
-
+if(! OPENAI_API_KEY){alert("No zOpenAI API key found. Please set the OPENAI_API_KEY environment variable.")}
 const configuration = new Configuration({
     // apiKey: OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
-
 type otherParams = {
     model: string,
     temperature: number,
@@ -23,14 +16,9 @@ type otherParams = {
     stop?: string[],
     additional_prompt?: string,
 };
-
 async function getPromptResponse(prompt : string, otherParams : otherParams) {
     const { model, temperature, max_tokens, top_p, frequency_penalty, presence_penalty, additional_prompt, stop } = otherParams;
-
-    if (additional_prompt) {
-        prompt = additional_prompt + prompt;
-    }
-
+    if(additional_prompt){prompt=additional_prompt+prompt}
     const completion = await openai.createCompletion({
         model,
         prompt,
@@ -41,15 +29,11 @@ async function getPromptResponse(prompt : string, otherParams : otherParams) {
         presence_penalty,
         stop,
     });
-
     return completion.data.choices[0].text;
-} 
-
+}
 export default defineEventHandler( async (event) => {
     const body = await readBody(event);
     const { prompt, otherParams } = body;
-
     const response = await getPromptResponse(prompt, otherParams);
-
     return response;
-});
+})
