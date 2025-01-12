c8b86d8b4f1cecedc498775f5a9118adac594f05
Erin Dunn Halligan <erin@designcandy.com>
2025-01-12 05:41:25 -0600

    Update q_a.vue

--- pages/openAI/q_a.vue
+++ pages/openAI/q_a.vue
@@ -11,29 +11,14 @@
 
 <template>
     <div class="row">
-        <div class="col-12 col-md-5 order-3 order-md-1">
-            <OpenAIForm :params="params" />
-        </div>
-
-        <div class="col-1 d-none d-md-block text-center order-md-2">
-            <div class="vr h-100 my-4"></div>
-        </div>
-
-        <div class="col-12 d-block d-md-none text-center order-2">
-            <hr class="w-100 mx-auto" />
-        </div>
-
+        <div class="col-12 col-md-5 order-3 order-md-1"><OpenAIForm :params="params" /></div>
+        <div class="col-1 d-none d-md-block text-center order-md-2"><div class="vr h-100 my-4"></div></div>
+        <div class="col-12 d-block d-md-none text-center order-2"><hr class="w-100 mx-auto" /></div>
         <div class="col-12 col-md-6 order-1 order-md-3">
-            <h5 class="mt-3 text-secondary">
-                Answer questions based on existing knowledge.
-            </h5>
-        
-            <h2>
-                Example :
-            </h2>
-        
+            <h5 class="mt-3 text-secondary">Answer questions based on existing knowledge.</h5>
+            <h2>Example:</h2>
             <div class="p-3 bg-light border rounded-3 example">
-                I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".
+                I am a question answering bot. If you ask me a question that is nonsense, I will respond with "Unknown".
                 <br />
                 Q: What is human life expectancy in the United States? <br/>
                 A: Human life expectancy in the United States is 78 years.
@@ -66,9 +51,4 @@
     </div>
 </template>
 
-<style scoped>
-    .example {
-        max-height: 35rem;
-        overflow: auto;
-    }
-</style>
+<style scoped>.example { overflow:auto; max-height:35rem; }</style>
